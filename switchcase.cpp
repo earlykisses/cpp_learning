382643b4c4ef7a184f74e80025fa1831f12d051a
PIYUSH KUMAR <piyushchilling@gmail.com>
2026-06-17 01:20:39 +0530

    Add switch case example in switchcase.cpp

---
+++ switchcase.cpp
@@ -0,0 +1,18 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+int num;
+cin>>num;
+cout<<endl;
+switch(num){
+    case(1):cout<<"first"<<endl;
+    break;
+    case(2) :
+    cout<<"second"<<endl;
+    break;
+}
+cout<<endl;
+return 0;
+}
+
