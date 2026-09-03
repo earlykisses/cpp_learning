92232ca3df7b52a74cc1ccab72036c16983f4e04
PIYUSH KUMAR <piyushchilling@gmail.com>
2026-09-03 23:20:12 +0530

    Time: 6 ms (52.36%), Space: 165.7 MB (87.43%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,23 @@
+class Solution { 
+public: 
+    bool uniformArray(vector<int>& nums1) { 
+        
+        int minOdd = INT_MAX;
+        int minEven = INT_MAX;
+
+        for (int x : nums1) {
+            if (x % 2 == 0)
+                minEven = min(minEven, x);
+            else
+                minOdd = min(minOdd, x);
+        }
+
+        // All numbers are already even
+        if (minOdd == INT_MAX)
+            return true;
+
+        // To make all numbers odd,
+        // every even number needs a smaller odd number.
+        return minOdd < minEven;
+    } 
+};
