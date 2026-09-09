4d967a5e315eec8b83f14a821f492797bd69f62c
PIYUSH KUMAR <piyushchilling@gmail.com>
2026-09-09 09:28:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,24 +1,15 @@
-class Solution {
-public:
-    long long countCommas(long long n) {
-        long long ans = 0;
-
-        // Numbers with 4 to 6 digits have 1 comma
-        // 7 to 9 digits have 2 commas
-        // 10 to 12 digits have 3 commas, etc.
-        
-        long long start = 1000;
-        long long commas = 1;
-
-        while (start <= n) {
-            long long end = min(n, start * 1000 - 1);
-
-            ans += (end - start + 1) * commas;
-
-            start *= 1000;
-            commas++;
+    class Solution {
+    public:
+        long long countCommas(long long n) {
+            long long comma=0;
+            long long base=1000;
+            if(n<1000) return 0;
+            else{
+                while(base<=n){
+                comma+=(n-base+1);
+                base*=1000;}
+            }
+            
+            return comma;
         }
-
-        return ans;
-    }
-};
+    };
